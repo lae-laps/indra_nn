909bd207fb6581a257961fcb484992cf38c87249
laelaps <77897569+lae-laps@users.noreply.github.com>
2022-12-11 00:20:09 +0100

    rust

---
+++ indra/target/debug/deps/indra-870f7a1109bf1a52.d
@@ -0,0 +1,5 @@
+/home/arch/workspace/indra_ann/indra/target/debug/deps/indra-870f7a1109bf1a52.rmeta: src/main.rs
+
+/home/arch/workspace/indra_ann/indra/target/debug/deps/indra-870f7a1109bf1a52.d: src/main.rs
+
+src/main.rs:
